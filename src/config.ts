e1f75ec6d76433f60d5db2bc92fe894d2f909b9c
franklin <116642775+luo2004@users.noreply.github.com>
2025-03-26 20:39:52 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 export let PROXIES: Proxy[] = [
   {
         domain: "easy-reverse-proxy.vercel.app",
-        url: "https://github.com/",
+        url: "https://google.com/",
         enable: true
     }
 ];
