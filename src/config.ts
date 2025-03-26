9ee6fc80d74affdc4d4e4f35975704701b1a9bb8
franklin <116642775+luo2004@users.noreply.github.com>
2025-03-26 20:35:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,10 @@ export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+  {
+        domain: "easy-reverse-proxy",
+        url: "https://github.com/",
+        enable: true
+    }
+];
